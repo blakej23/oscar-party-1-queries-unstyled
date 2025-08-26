bb190afc977b5a93e276bb0bcb6ec38d67ee647e
blakej23 <blakejones1991@gmail.com>
2025-08-26 04:32:31 +0000

    Actors mostly built

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,14 +1,17 @@
-Class ActorsController < ApplicationController
+class ActorsController < ApplicationController
 
   def index
-
+    @list_of_actors = Actor.all
 
     render({ :template => "actors_templates/index"})
   end
 
   def show
+    the_id = params.fetch("path_id")
 
+    matching_actors = Actor.where({ :id => the_id })
 
+    @the_actor = matching_actors.at(0)
 
     render({ :template => "actors_templates/show"})
 
